5abd9dcef3d1a1d9b07f514ac7dd2fcd1fff9661
vishand <vishandr@gmail.com>
2022-07-25 00:12:35 +0300

    finished Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import './header.css'
+// import Cart from './Cart'
 
 const nav = ['women', 'men', 'kids'];
-const navLinks = nav.map((link) => <li key={link}>{link.toUpperCase()}</li>)
+const navLinks = nav.map((link) => <li key={link}><a href="/">{link.toUpperCase()}</a></li>)
 
 class Header extends React.Component{
   render(){
@@ -11,10 +12,23 @@ class Header extends React.Component{
       <header className="header">
        
         <nav>
-          <ul>{navLinks}</ul>
+          <div>
+            <ul>{navLinks}</ul>
+          </div>
+          {/* <div className="logo"> */}
+            <img src="https://img.icons8.com/material/344/shopaholic.png" alt="logo" className="logo"/>
+          {/* </div> */}
+          <div className="cart">
+            <select name="currency">
+              <option className="option" selected disabled>$</option>
+              <option className="option" value="usd">$ USD</option>
+              <option className="option" value="eur">€ EUR</option>
+              <option className="option" value="jpy">¥ JPY</option>
+            </select>
+            <img src="https://img.icons8.com/windows/344/shopping-cart.png" alt="cart" className="cartLogo"/>
+          </div>
         </nav>
-        {/* here will be original logo */}
-        <img src="https://img.icons8.com/material/344/shopaholic.png" alt="logo" className="logo"/>
+        
         
         
       </header>
