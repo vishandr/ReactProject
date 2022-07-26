68f809ebd9c847c22a92c63932339e810d68dba8
vishand <vishandr@gmail.com>
2022-07-26 16:10:21 +0300

    modified App from function to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
+import React from "react";
 import Header from "./components/Header/Header";
 import Categories from "./components/Categories/Categories";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Categories />
-    </>
+class App extends React.Component {
+  render(){
+    return (
+      <>
+        <Header />
+        <Categories />
+      </>
 
-  );
+    );
+  }
 }
 
 export default App;
