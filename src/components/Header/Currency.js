8478c2e579b5541c25e57108046043e6cb59e032
vishand <vishandr@gmail.com>
2022-08-03 19:41:31 +0300

    add possibility to change state for Curreny

--- src/components/Header/Currency.js
+++ src/components/Header/Currency.js
@@ -1,47 +1,25 @@
 import React from "react";
 import { useQuery } from '@apollo/client'
 import { GET_CURRENCY } from '../Apollo/request'
+import { useState } from 'react'
+
 
 function Currency(){
+    const [value, setValue] = useState('');
+    
+    function changeSelect(event) {
+        setValue(event.target.value);
+     }
+
     const { loading, error, data} = useQuery(GET_CURRENCY);
     if (loading) return <h2>Loading...</h2>;
     if (error) return <p>Error :</p>;
     return(
-        <select>
+        <select value={value} onChange={(event) => setValue(event.target.value)}>
             {data.currencies.map(({label, symbol}) => 
             <option className="option" key={label} value= {label}> {symbol} {label} </option>)}
         </select>
     )
 }
-
-
-// class  extends React.Component{
-//     constructor (props){
-//         super(props)
-//         this.state = {
-//             currencies: 
-//                 {
-//                 "label": "USD",
-//                 "symbol": "$"
-//                 }
-//         }
-//         componentDidMount(){
-//             fetch("http://localhost:4000/")
-//             .then((response) => console.log(response))
-//             // .then(console.log)
-//             // .then(currenciesList => {
-//             //     this.setState({ currencies: currenciesList });
-//             // });
-//             }
-//         }
-//         // console.log(fetchCurrencies)
-//     }
-
-    
-//     render(){
-        
-//     }
-// };
-
 
 export default Currency
