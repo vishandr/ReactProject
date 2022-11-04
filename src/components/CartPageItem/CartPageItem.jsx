8f87fd0ec033769889cb4d29900571faff704ea9
vishand <vishandr@gmail.com>
2022-11-04 14:18:07 +0200

    add carousel to CartPage

--- src/components/CartPageItem/CartPageItem.jsx
+++ src/components/CartPageItem/CartPageItem.jsx
@@ -1,14 +1,36 @@
 import './CartPageItem.css'
 import React from 'react';
 import {CartContext} from '../../contexts/cart-context'
+import { ReactComponent as Carret } from '../../assets/carret.svg';
 
 class CartPageItem extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            i: 0
+        }
+    }
     render(){
+        
         const {gallery, attributes, prices, name, brand, quantity } = this.props.cartItem
         const { currentCurrencySymbol, currentCurrencyLabel } = this.context
         const addItemHandler = () => this.context.addToCart(this.props.cartItem);
         const removeItemHandler = () => this.context.removeItemFromCart(this.props.cartItem);
-
+    
+        const changeImagePrev = () =>{
+            console.log(this.state.i);
+            (this.state.i === 0) ? 
+            this.setState({i: gallery.length-1}): 
+            this.setState({i: this.state.i - 1})
+        }
+        
+        const changeImageNext = () =>{
+            (this.state.i === gallery.length-1) ? 
+            this.setState({i: 0}): 
+            this.setState({i: this.state.i + 1})
+        }
+        
+        console.log({gallery})
         return(
             <div className='cart-item-container'>
                 <div className='cart-item-description-container'>
@@ -41,8 +63,15 @@ class CartPageItem extends React.Component{
                 </div>
 
                 <div className='cart-item-image-container'>
-                <img src={gallery[0]} alt="" />
-                
+                    <img src={gallery[this.state.i]} alt={name} />
+                    <div className='cart-image-carret-box'>
+                        <div className='cart-image-carret'
+                            onClick={changeImagePrev}>
+                        <Carret />
+                        </div>
+                        <div className='cart-image-carret cart-image-carret-right'
+                            onClick={changeImageNext}><Carret /></div>
+                    </div>
                 </div>
             </div>
         )
